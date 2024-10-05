0362d46690049a97daffab18c1e5921191d102f1
Francisco Alvarez <faovenezuela@gmail.com>
2024-10-05 06:23:27 -0300

    Add ClientStatus

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,10 +13,39 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        //$this->call(ProcessSeeder::class); 
+        //$this->call(ProcessSpecialtiesSeeder::class);
+        //Category
+        //$this->call(CategoriesSeeder::class);
+        //Datos Asociados
+        //$this->call(AssociateSeeder::class);
+        //Client Status
+        $this->call(ClientStatusSeeder::class);
+        //Ciudades    
+        //$this->call(CitieSeeder::class);
+        //Countries
+        //$this->call(CommunicationSeeder::class);
+        //Facturas
+        //$this->call(InvoiceSeeder::class);
+        //Plazos de Entregas
+        //$this->call(ProductsSeeder::class);
+        //Servicios
+        //$this->call(ServiceSeeder::class);
+        //tipo de Comunicacion
+        //$this->call(TypeCommunicationSeeder::class);
+        //Tipo de Servicios
+        //$this->call(TypeServiceSeeder::class);
+        //Tipo de Products
+        //$this->call(TypeProductsSeeder::class);
+        //Motivo de la comunicación
+        //$this->call(ReasonSeeder::class);
+        //Dias libres
+        //$this->call(HolidaysTableSeeder::class);
         //Countries
         $this->call(CountriesSeeder::class);
         //Civil_Status
         $this->call(CivilStatusSeeder::class);
+        
         //Associates
         $this->call(AssociateSeeder::class);
         //Datos de Usuarios
